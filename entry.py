2e9b4c801b019f377581b70d1bd0538e5ea74d5d
Leo Kosta <kostaleonard@gmail.com>
2023-04-15 17:21:07 -0400

    Added OpenAI API key
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

--- entry.py
+++ entry.py
@@ -1,14 +1,16 @@
 """An entrypoint for the action."""
 
 import glob
+import os
 import subprocess
 import sys
 
+os.environ["OPENAI_API_KEY"] = sys.argv[1]
 clean_args = []
-if sys.argv[1] != "--fail_under=":
+if sys.argv[2] != "--fail_under=":
     clean_args.append(sys.argv[1])
 expanded_filenames = []
-for filenames_with_potential_spaces in sys.argv[2:]:
+for filenames_with_potential_spaces in sys.argv[3:]:
     for filename in filenames_with_potential_spaces.strip().split(" "):
         filenames = glob.glob(filename, recursive=True)
         expanded_filenames.extend(filenames)
