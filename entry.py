9b93b69d2c9577049fbb5c1b155d4b38c5510990
Leo Kosta <kostaleonard@gmail.com>
2023-04-15 17:06:55 -0400

    Added preprocessing
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

--- entry.py
+++ entry.py
@@ -8,9 +8,10 @@ clean_args = []
 if sys.argv[1] != "--fail_under=":
     clean_args.append(sys.argv[1])
 expanded_filenames = []
-for filename in sys.argv[2:]:
-    filenames = glob.glob(filename, recursive=True)
-    expanded_filenames.extend(filenames)
+for filenames_with_potential_spaces in sys.argv[2:]:
+    for filename in filenames_with_potential_spaces.strip().split(" "):
+        filenames = glob.glob(filename, recursive=True)
+        expanded_filenames.extend(filenames)
 clean_args += expanded_filenames
 process = subprocess.run(["backseat-driver"] + clean_args)
 exit(process.returncode)
